3e40d08455581bbd766699a910a0b654fdc2ac29
Mubarak Mohamed <moubarakmohamed81@yahoo.fr>
2025-02-07 11:39:16 +0100

    Change ip adress

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["16.171.197.62"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["3.9.134.192"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
